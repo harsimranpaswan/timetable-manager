6832fd1794b195e8ae67be98b19074272af306d0
Harsimran <harsimran.iit@gmail.com>
2022-03-17 18:22:38 +0530

    added tabs layout, internal components not added yet

---
+++ app/src/main/java/com/workshop/ttm/FragmentAdapter.java
@@ -0,0 +1,37 @@
+package com.workshop.ttm;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+
+    private String[] title= new String[]{"Tasks","Postponed","Add new task","Status","Settings"};
+    public FragmentAdapter(FragmentActivity fragact){
+        super(fragact);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0: return new tasks();
+            case 1: return new postponed();
+            case 2: return new addtask();
+            case 3: return new status();
+            case 4: return new settings();
+        }
+        return new tasks();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+}
--- app/src/main/java/com/workshop/ttm/MainActivity.java
+++ app/src/main/java/com/workshop/ttm/MainActivity.java
@@ -1,14 +1,36 @@
 package com.workshop.ttm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tablayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter adap;
+    private String[] title= new String[]{"Tasks","Postponed","Add new task","Status","Settings"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        tablayout = findViewById(R.id.tablayout);
+        viewPager2 = findViewById(R.id.viewPager2);
+        adap=new FragmentAdapter(this);
+
+        viewPager2.setAdapter(adap);
+
+       //new TabLayoutMediator(tablayout, viewPager2, ((tab, position) -> tab.setText(title[position]))).attach();
     }
 }
